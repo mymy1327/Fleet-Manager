10f244ca46655ac6ede8f2791de8be3190b93b66
matkolo1 <matej.kor@email.cz>
2026-09-15 10:13:26 +0300

    completed checklist item database API

--- assets/config.php
+++ assets/config.php
@@ -1,7 +1,8 @@
 <?php
 // DB login info
 $servername = "10.1.17.108:3306";
-$username = 'root';
+//$servername = "127.0.0.1:3306";
+$username = "root";
 $password = "root";
 $dbname = "fleet-manager";
 
@@ -10,4 +11,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->set_charset("utf8");
 if ($conn->connect_error) {
     die("Error connecting to DB: " . $conn->connect_error);
+}
+
+/**
+ * exit this script
+ * @param int $code response code
+ * @param any $response response to be returned
+ */
+function heaDie($code, $response = null)
+{
+    http_response_code($code);
+    die($response);
 }
--- databaseAPI/vehicleChecklists.php
+++ databaseAPI/vehicleChecklists.php
@@ -11,32 +11,44 @@
 session_start();
 require "../assets/config.php";
 
-//$data = json_decode(file_get_contents("php://input"), true);
+$data = json_decode(file_get_contents("php://input"), true);
 
+/**
+ * returns all checklist items for vehicle
+ * @param mysqli $conn connection to database
+ * @param int $id vehicle id
+ * @return json|false list of checklist items | false on failure
+ */
+function getAllChecklistItemsForVehicle($conn, $id)
+{
+    $stmt = $conn->prepare(
+        "SELECT c.id_checklists, c.name,  c.description FROM `vehicle_checklists` vc JOIN `checklists` c ON vc.id_checklists = c.id_checklists WHERE vc.id_vehicles = ?",
+    );
+    $vehicleId = (int) $id;
+    $stmt->bind_param("i", $vehicleId);
+    if (!$stmt->execute()) {
+        return false;
+    }
+    $result = $stmt->get_result();
+    if (!$result) {
+        return false;
+    }
+    return json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_NUMERIC_CHECK);
+}
+
+//echo $_SERVER["REQUEST_METHOD"], " ", $_GET["id_vehicles"];
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
-        if (isset($_GET["vehicle"])) {
-            $stmt = $conn->prepare(
-                "SELECT c.id_checklists, c.name,  c.description FROM `vehicle_checklists` vc JOIN `checklists` c ON vc.id_checklists = c.id_checklists WHERE vc.id_vehicles = ?",
-            );
-            $get = (int) $_GET["vehicle"];
-            $stmt->bind_param("i", $get);
-            if (!$stmt->execute()) {
-                http_response_code(400);
-                die();
+        if (isset($_GET["id_vehicles"])) {
+            $return = getAllChecklistItemsForVehicle($conn, $_GET["id_vehicles"]);
+            if (!$return) {
+                heaDie(404);
             }
-            $result = $stmt->get_result();
-            if (!$result) {
-                http_response_code(404);
-                die();
-            }
-            http_response_code(200);
-            die(json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_NUMERIC_CHECK));
+            heaDie(200, $return);
+        } else {
+            heaDie(400);
         }
-        http_response_code(400);
-        die();
     default:
-        http_response_code(405);
-        die();
+        heaDie(405);
 }
 ?>
