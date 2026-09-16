dbd70734c67097fd03492c71d3b448a4b4a3193f
SeSignal <sesignal@tuta.io>
2026-09-16 12:22:50 +0300

    again picutre

--- VehicleList/vehiclelist.js
+++ VehicleList/vehiclelist.js
@@ -33,7 +33,7 @@ function getVehicleDetails(vehicleId) {
 
                 //set the image
                 let picture = document.createElement("img")
-                picture.src = `/api/files/${(Number(number)+1)}`; 
+                picture.src = `/api/files/${(parseInt(x)+1)}`; 
                 //set the div for everything else
                 let vehicledetails = document.createElement("div")
                 vehicledetails.classList.add("vehicle-details")
