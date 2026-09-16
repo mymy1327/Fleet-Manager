62e5f95d163509852c71ff064d71ebb07d3f69ec
SeSignal <sesignal@tuta.io>
2026-09-16 12:22:50 +0300

    again picutre

--- VehicleList/vehiclelist.js
+++ VehicleList/vehiclelist.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 //set the image
                 let picture = document.createElement("img")
-                picture.src = `/api/files/${(Number(number)+1)}`; 
+                picture.src = `/api/files/${(parseInt(x)+1)}`; 
                 //set the div for everything else
                 let containing = document.createElement("div")
                 containing.classList.add("panel")
