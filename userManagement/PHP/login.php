8daffe13dbe3184ca6f4cab17f6df8362353fab6
mymy1327 <my.lengoctra@edu.hyria.fi>
2026-09-21 12:25:06 +0300

    css style for login page

--- userManagement/PHP/login.php
+++ userManagement/PHP/login.php
@@ -49,21 +49,32 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </head>
 <body>
     <main id="app" class="page">
+
         <div class="page-heading">
-            <div>
-                <h1>Login</h1>
-                <p class="subtitle">Enter your username and password for continue.</p>
-            </div>
+            <h1>Login</h1>
+            <p class="sub">
+                Enter your username and password for continue.
+            </p>
         </div>
-	<label for="username">Username:</label><br>
-	<input type="text" name="username" id="username"><br>
-	<label for="password">Password:</label><br>
-	<input type="password" name="password" id="password">
-	<input type="checkbox" name="passwordEye" id="passwordEye">
-	<label for="passwordEye">Show password</label><br>
-	<button id='login' class='button'>Login</button>
+
+        <div class="login-container">
+
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+
+            <div class="show-password">
+                <input type="checkbox" name="passwordEye" id="passwordEye">
+                <label for="passwordEye">Show password</label>
+            </div>
+
+            <button id="login" class="button">login</button>
+
+        </div>
+
     </main>
- <br>
 </body>
 <script src="../JS/login.js"></script>
 </html>
