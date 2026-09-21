93ee24e85aec009540fb15a1607bf92d27f39497
matkolo1 <matej.kor@email.cz>
2026-09-21 12:43:15 +0300

    last option

--- vehicle/index.php
+++ vehicle/index.php
@@ -3,40 +3,28 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Origin");
 
-//require "../assets/sharedUserFunctions.php";
-
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $parts = explode("/", trim($path, "/"));
 
 $code = $parts[1] ?? null;
-echo $code;
 
 if (!$code) {
     http_response_code(400);
     die("Missing vehicle code");
 }
+?>
 
-$url = "https://developmenterasmus.kolojar.cz/api/vehicles?code=" . urlencode($code);
+<script>
+const restapi = "http://127.0.0.1:5501";
+const code = "<?php echo $code; ?>";
+console.log(code);
+const xhr = new XMLHttpRequest();
+xhr.open("GET", restapi + "/api/vehicles?code=" + code, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onload = () => {
+    let id = JSON.parse(xhr.responseText)[0]["id_vehicles"];
+    window.location = "https://developmenterasmus.kolojar.cz/inspection-student-form/index.html?id=" + id;
+};
+xhr.send();
 
-$rawResponse = file_get_contents($url);
-echo "<br>" . $rawResponse;
-
-$response = json_decode($rawResponse, true);
-
-if (json_last_error() !== JSON_ERROR_NONE) {
-    http_response_code(500);
-    die("Invalid JSON: " . json_last_error_msg());
-}
-
-if (!isset($response[0]["id_vehicles"])) {
-    http_response_code(404);
-    die("Vehicle not found");
-}
-
-$id = $response[0]["id_vehicles"];
-echo "<br>" . $id;
-
-$newUrl = "https://developmenterasmus.kolojar.cz/inspection-student-form/index.html?id=" . urlencode($id);
-
-//header("Location: " . $newUrl, true, 302);
-exit();
+</script>
