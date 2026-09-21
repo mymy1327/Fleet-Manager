352aa9ed30d1c936e515c4cf8fa2460174cdb1d7
matkolo1 <matej.kor@email.cz>
2026-09-21 12:11:07 +0300

    another test

--- vehicle/index.php
+++ vehicle/index.php
@@ -5,7 +5,15 @@ require "../assets/sharedUserFunctions.php";
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $uri = array_slice(explode("/", $path), 2);
 
-$response = SendRequestToAPI("/vehicles?code=" . $uri[0]);
+$ch = curl_init("https://developmenterasmus.kolojar.cz/api/vehicles?code=" . $uri[0]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = json_decode(curl_exec($ch), true);
+curl_close($ch);
+
+if (!$response) {
+    echo "none";
+    die();
+}
 $id = $response[0]["id_vehicles"];
 $newUrl = "https://developmenterasmus.kolojar.cz/inspection-student-form/index.html?id=" . $id;
 print json_encode($response) . "\n";
