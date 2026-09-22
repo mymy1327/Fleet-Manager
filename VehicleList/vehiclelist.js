b8e503cb0fcce01ff19a8537dac36009fd507fdc
neurodivergented <arttu.hietanen@edu.hyria.fi>
2026-09-22 09:03:31 +0300

    Merge

--- VehicleList/vehiclelist.js
+++ VehicleList/vehiclelist.js
@@ -3,13 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
 let carlist = document.getElementById("vehicle")
 
 const xhr = new XMLHttpRequest();
-const ip = "10.1.17.107:5503"
+const ip = "developmenterasmus.kolojar.cz"
 function getAllInspections(changing, vehicleId = null) {
         const xhr = new XMLHttpRequest();
         xhr.open("GET", "/api/inspections?id_vehicles=" + vehicleId, true); // ?id_vehicles can be removed
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onload = () => {
-            
+
             unintelligeble = JSON.parse(xhr.responseText)
             let temp = 0
             for (x in unintelligeble){
@@ -30,7 +30,7 @@ function getVehicleDetails(vehicleId) {
         xhr.open("GET", `http://${ip}/api/vehicles`, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onload = () => {
-            
+
             console.log(`/api/vehicles`)
             console.log(xhr.status)
             console.log(1)
@@ -42,9 +42,6 @@ function getVehicleDetails(vehicleId) {
             for (x in intelligeble){
                 // create id
                 let newId = /item-${i+1}/;
-
-
-                
 
                 //create div
                 let newArticle = document.createElement("article")
@@ -62,15 +59,18 @@ function getVehicleDetails(vehicleId) {
                 //set the div for everything else
                 let vehicledetails = document.createElement("div")
                 vehicledetails.classList.add("vehicle-details")
-                
+
                 let vehicleheading = document.createElement("div")
                 vehicleheading.classList.add("vehicle-heading")
                 let vehiclemodel = document.createElement("h2")
                 vehiclemodel.classList.add("vehicle-model")
                 vehiclemodel.textContent = intelligeble[x].name
                 let vehiclestatus = document.createElement("span")
-                vehiclestatus.classList.add("vehicle-status")
+                vehiclestatus.classList.add(`vehicle-status`)
+                vehiclestatus.classList.add(`${intelligeble[x].state}`)
                 vehiclestatus.textContent = intelligeble[x].state
+                // to be done 
+
 
                 let vehicletype = document.createElement("p")
                 vehicletype.textContent = intelligeble[x].type
@@ -78,16 +78,21 @@ function getVehicleDetails(vehicleId) {
                 //set the type
                 let vehicledistance = document.createElement("p")
                 vehicledistance.classList.add("vehicle-distance")
-                vehicledistance.textContent = getAllInspections(vehicledistance, intelligeble[x].id_vehicles)
+                if (typeof(intelligeble[x].km) === "number"){
+                    vehicledistance.textContent = `${intelligeble[x].km} km`
+                }else{
+                    vehicledistance.textContent = "km ei saatavilla"
+                }
+                
 
                 let vehiclelicense = document.createElement("span")
                 vehiclelicense.classList.add("vehicle-license")
                 vehiclelicense.textContent = intelligeble[x].license_plate
-                
+
                 let vehiclecode = document.createElement("p")
                 vehiclecode.classList.add("vehicle-code")
                 vehiclecode.textContent = intelligeble[x].code
-                
+
                 //append in div
                 newArticle.appendChild(clickableconnection)
                 clickableconnection.appendChild(picture)
@@ -100,13 +105,13 @@ function getVehicleDetails(vehicleId) {
                 vehicledetails.appendChild(vehiclelicense)
                 vehicledetails.appendChild(vehiclecode)
                 placeforit.appendChild(newArticle)
-                
-                
+
+
                 // add an extra 1fr for every div created
                 neededfr += "1fr "
-                
+
             }
-            carlist.style.gridTemplateColumns = neededfr/2 
+            carlist.style.gridTemplateColumns = neededfr/2
             console.log(carlist.style.gridTemplateColumns)
             carlist.style.gridTemplateRows = neededfr/4
             //document.getElementById("demo1").innerHTML = xhr.status + "<br>" + xhr.responseText;
@@ -115,78 +120,7 @@ function getVehicleDetails(vehicleId) {
     }
 
 
-function getAllVehicles() {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", `http://${ip}/api/vehicles`, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = () => {
-        console.log(getVehicleDetails("q6aus0cdi9r"))
-        console.log(xhr.status)
-        console.log(1)
-        console.log(xhr.responseText)
-        intelligeble = JSON.parse(xhr.responseText)
-        console.log(intelligeble)
-        const placeforit = document.getElementById("car");
-        let neededfr = ""
-        for (x in intelligeble){
-            // create id
-            let newId = /item-${i+1}/;
-            //create div
-            let newDiv = document.createElement("div")
-            newDiv.classList.add("card-garage");
-            // set id
-            newDiv.id = newId;
-                
-            //set the imagebox
-            let picturebox = document.createElement("div")
-            picturebox.textContent = "heloeheohloerlherholershol"
-            picturebox.classList.add("panel")
 
-            //set the image
-            let picture = document.createElement("img")
-            picture.src = `http://${ip}/api/files/${(parseInt(intelligeble[x].id_files))}`;
-            picture.style.width = "80px"
-            picture.style.height = "80px" 
-            //set the div for everything else
-            let containing = document.createElement("div")
-            containing.classList.add("panel")
-            let name = document.createElement("p")
-            name.textContent = intelligeble[x].name
-            name.classList.add("card-value")
-            //set the type
-            let type = document.createElement("p")
-            type.classList.add("card-value")
-            type.textContent = intelligeble[x].type
-
-            let licenseplate = document.createElement("p")
-            licenseplate.textContent = intelligeble[x].license_plate
-                
-            let code = document.createElement("p")
-            code.textContent = intelligeble[x].code
-            
-            //append in div
-            newDiv.appendChild(picture)
-            newDiv.appendChild(containing)
-            containing.appendChild(name)
-            containing.appendChild(type)
-            containing.appendChild(licenseplate)
-            containing.appendChild(code)
-            placeforit.appendChild(newDiv)
-         
-             
-             // add an extra 1fr for every div created
-             neededfr += "1fr "
-             
-        }
-        carlist.style.gridTemplateColumns = neededfr
-        console.log(carlist.style.gridTemplateColumns)
-        //document.getElementById("demo1").innerHTML = xhr.status + "<br>" + xhr.responseText;
-    };
-    xhr.send(null);
-}
 
     getVehicleDetails();
 })
-
-
-
--- assets/sharedUserFunctions.php
+++ assets/sharedUserFunctions.php
@@ -1,6 +1,13 @@
 <?php
+require_once __DIR__ . "/../errorPages/PHP/errorManager.php";
 $API = "https://developmenterasmus.kolojar.cz/api";
-function CheckAccessSession(array $roles)
+/**
+ * Checks if user id in session is possible to login
+ * @param array $roles Array of allowed roles
+ * @param bool $displayError If should render error page or not
+ * @return bool If access allowed
+ */
+function CheckAccessSession(array $roles, bool $displayError = true): bool
 {
     //Start session if needed
     if (session_status() != PHP_SESSION_ACTIVE) {
@@ -8,23 +15,40 @@ function CheckAccessSession(array $roles)
     }
 
     //Set session value if needed
-    if (!isset($_SESSION["login"]) || $_SESSION["login"] == "") {
-        $_SESSION["login"] = "-1";
+    if (!isset($_SESSION["login"]) || $_SESSION["login"] == "" || $_SESSION["login"] == "-1") {
+        if (isset($_COOKIE["remember_me_username"]) && isset($_COOKIE["remember_me_password"])) {
+            $result = HandleLogin( $_COOKIE["remember_me_username"],$_COOKIE["remember_me_password"],true,null);
+            if($result["code"] === 200) {
+                $_SESSION["login"]  = $result["id"];
+            } else {
+                 $_SESSION["login"] = "-1";
+            }
+        } else {
+            $_SESSION["login"] = "-1";
+        }
     }
 
     //Check access
-    $result = CheckAccess($_SESSION["login"], $roles);
+    $result = CheckAccess($_SESSION["login"], $roles, $displayError);
     if ($result === true) {
-        return;
+        return true;
     }
 
     //Deny on error
-    if($_SESSION["login"] == "-1") {
-        HandleError(401);
-        die();
+    if ($_SESSION["login"] == "-1") {
+        if ($displayError) {
+            HandleError(401);
+            die();
+        } else {
+            return false;
+        }
     }
-    HandleError($result);
-    die();
+    if ($displayError) {
+        HandleError($result);
+        die();
+    } else {
+        return false;
+    }
 }
 
 function CheckAccess(int $user, array $roles): int|true
@@ -43,17 +67,116 @@ function CheckAccess(int $user, array $roles): int|true
 }
 
 /**
- * Sends request to API
- * @param path Path at API URL
- * @param method HTTP Method
- * @param body Data for method body - will be converted to JSON
+ * Handles login of user
+ * @param string $username Username
+ * @param string $password Password
+ * @param bool $rememberMe Remember user
+ * @param string|null $next Next URL, can be null
+ * @param mixed Result of login
+ */
+function HandleLogin(string $username, string $password, bool $rememberMe, string|null $next): mixed {
+    //Handle login - Get user info form API
+    $users = SendRequestToAPI("/users","GET");
+    if($users === false) {
+        $result = [];
+        $result["code"] = 503;
+        $result["message"] = "API not available!";
+        return $result;
+    }
+
+    //Find user
+    foreach ($users as $user) {
+        if ($user["username"] == $username) {
+            //Verify password
+            if (password_verify($password, $user["password"])) {
+                //Remember me
+                if($rememberMe) {
+                setcookie('remember_me_username', $username, [
+                    'expires'  => time() + 30 * 24 * 3600,
+                    'path'     => '/',
+                    'secure'   => true,
+                    'httponly' => true,
+                    'samesite' => 'Lax',
+                ]);
+                setcookie('remember_me_password',$password, [
+                    'expires'  => time() + 30 * 24 * 3600,
+                    'path'     => '/',
+                    'secure'   => true,
+                    'httponly' => true,
+                    'samesite' => 'Lax',
+                ]);
+                }
+
+                //Redirect to valid password
+                $_SESSION["login"] = $user["id_users"];
+                $result = [];
+                if($next !== null) {
+                    if($user["role"] == "admin") {
+                        $result["next"] = "./admin.php";
+                    } else if($user["role"] == "user") {
+                        $result["next"] = "../../userDashboard/PHP/index.php";
+                    }
+                } else {
+                    $result["next"] = $next;
+                }
+                $result["id"] = $user["id_users"];
+                $result["code"] = 200;
+                $result["message"] = "";
+                return $result;
+            }
+            //Clear cookies
+            setcookie('remember_me_username', '', [
+                'expires'  => time() - 30 * 24 * 3600,
+                'path'     => '/',
+                'secure'   => true,
+                'httponly' => true,
+                'samesite' => 'Lax',
+            ]);
+            setcookie('remember_me_password', '', [
+                'expires'  => time() - 30 * 24 * 3600,
+                'path'     => '/',
+                'secure'   => true,
+                'httponly' => true,
+                'samesite' => 'Lax',
+            ]);
+            $result = [];
+            $result["code"] = 401;
+            $result["message"] = "Invalid password!";
+            return $result;
+        }
+    }
+    //Clear cookies
+    setcookie('remember_me_username', '', [
+        'expires'  => time() - 30 * 24 * 3600,
+        'path'     => '/',
+        'secure'   => true,
+        'httponly' => true,
+        'samesite' => 'Lax',
+    ]);
+    setcookie('remember_me_password', '', [
+        'expires'  => time() - 30 * 24 * 3600,
+        'path'     => '/',
+        'secure'   => true,
+        'httponly' => true,
+        'samesite' => 'Lax',
+    ]);
+    $result = [];
+    $result["code"] = 404;
+    $result["message"] = "User not found!";
+    return $result;
+}
+
+/**
+ * Sends request to URL
+ * @param string $url Target URL
+ * @param string $method HTTP Method
+ * @param mixed $body Data for method body - will be converted to JSON
  * @return false|mixed Returns false when error or JSON object as mixed on success
  */
-function SendRequestToAPI(string $path, string $method = "GET", mixed $body = null): mixed
+function SendRequestToURL(string $url, string $method = "GET", mixed $body = null): mixed
 {
     //Prepare cURL
-    global $API;
-    $curl = curl_init($API . $path);
+    $curl = curl_init($url);
 
     //Setup headers
     $headers = ["User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36", "Accept: application/json"];
@@ -73,11 +196,11 @@ function SendRequestToAPI(string $path, string $method = "GET", mixed $body = nu
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($method));
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 15);
 
     //Set payload
     if (!empty($payload)) {
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
     }
 
     // Execute request
@@ -96,39 +219,54 @@ function SendRequestToAPI(string $path, string $method = "GET", mixed $body = nu
     $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     curl_close($curl);
     if ($httpCode < 200 || $httpCode >= 300) {
-        error_log("API Error: HTTP status code " . $httpCode . " returned from " . $API . $path);
+        error_log("API Error: HTTP status code " . $httpCode . " returned from " . $url);
         return false;
     }
 
     //Decode responce
-    return json_decode($response,true);
+    return json_decode($response, true);
+}
+
+/**
+ * Sends request to API
+ * @param string $path Path at API URL
+ * @param string $method HTTP Method
+ * @param string $body Data for method body - will be converted to JSON
+ * @return false|mixed Returns false when error or JSON object as mixed on success
+ */
+function SendRequestToAPI(string $path, string $method = "GET", mixed $body = null): mixed
+{
+    global $API;
+    return SendRequestToURL($API . $path,$method, $body);
 }
 
 /**
  * Gets POST data
  * @return mixed Values
  */
-function GetPOSTData(): mixed {
-    return json_decode(file_get_contents('php://input'), true);
+function GetPOSTData(): mixed
+{
+    return json_decode(file_get_contents("php://input"), true);
 }
 
 /**
  * Converts absolute path to URL on this server
  * @param string $path Path to file
  */
-function PathToURL(string $path) {
+function PathToURL(string $path)
+{
     //Convert slashes
-    $path = str_replace('\\', '/', $path);
+    $path = str_replace("\\", "/", $path);
 
     //Convert to relative path
-    $relativePath = str_replace(str_replace('\\', '/',$_SERVER['DOCUMENT_ROOT']), '', $path);
+    $relativePath = str_replace(str_replace("\\", "/", $_SERVER["DOCUMENT_ROOT"]), "", $path);
 
     // Get request info
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http';
-    $host = $_SERVER['HTTP_HOST'];
+    $protocol = !empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on" ? "https" : "http";
+    $host = $_SERVER["HTTP_HOST"];
 
     //Build URL
-    return $protocol . '://' . $host . $relativePath;
+    return $protocol . "://" . $host . $relativePath;
 }
 
 /**
@@ -136,35 +274,31 @@ function PathToURL(string $path) {
  * @param int $code HTTP error code
  * @param string|null $message Status message, set to null for none
  * @param string|null $from Overwrite source URL
+ * @param string $lang Language of page
+ * @param bool $redirect If should redirect
  */
-function HandleError(int $code, string|null $message = null, string|null $from = null) {
+function HandleError(int $code, string|null $message = null, string|null $from = null, string $lang = "en", bool $redirect = false)
+{
     //Get paths
-    $path = __DIR__ . "/../errorPages/PHP/" . $code . ".php";
+    $path = __DIR__ . "/../errorPages/PHP/handleError.php";
     $url = PathToURL($path);
 
     //Chceck if from is null
-    if($from === null) {
-        $from = $_SERVER['REQUEST_URI'];
+    if ($from === null) {
+        $from = $_SERVER["REQUEST_URI"];
     }
 
-    //Check if message not empty
-    if($message === "") {
-        $message = null;
-    }
-
-    //Check if path exists
-    if(file_exists($path)) {
-        $url = "Location: " . $url . "?from=" . rawurlencode($from);
-        if($message !== null) {
+    //Handle redirect
+    if ($redirect === true) {
+        $url = "Location: " . $url . "?from=" . rawurlencode($from) . "&lang=" . rawurlencode($lang);
+        if ($message !== null) {
             $url .= "&message=" . urlencode($message);
         }
         header($url);
         die();
+    } else {
+        HandleErrorPageLocal(false, $code, $message, $from, $lang);
     }
-
-    //Invalid error page
-    echo "<h1>Error: " . $code . "</h1>";
-    echo "<p>" . $message . "</p>";
 }
 
 /**
@@ -173,13 +307,14 @@ function HandleError(int $code, string|null $message = null, string|null $from =
  * @param string|null $message Status message, set to null for none
  * @return string Echoes responce as JSON
  */
-function GenerateAPIError(int $code, string|null $message = null) {
+function GenerateAPIError(int $code, string|null $message = null)
+{
     http_response_code($code);
     $responce = [];
     $responce["code"] = $code;
-    if($message !== null) {
+    if ($message !== null) {
         $responce["message"] = $message;
     }
-    echo (json_encode($responce));
+    echo json_encode($responce);
     die();
 }
