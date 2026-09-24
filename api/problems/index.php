0405c8a85fe948b2ffef92791f76d7b669844067
matkolo1 <matej.kor@email.cz>
2026-09-24 11:11:42 +0300

    cors fix

--- api/problems/index.php
+++ api/problems/index.php
@@ -3,8 +3,8 @@
  * @var mysqli $conn
  */
 header("Content-Type: application/json");
-header("Allow: DELETE, PUT, PATCH");
-header("Access-Control-Allow-Methods: DELETE");
+header("Allow: *");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Origin");
 //session_start();
