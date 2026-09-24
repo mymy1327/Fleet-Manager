4a8d9a4e6cfb5e151177ebb447bcbaccd9f1d9b8
mymy1327 <my.lengoctra@edu.hyria.fi>
2026-09-24 20:57:53 +0300

    .

--- admin/user.php
+++ admin/user.php
@@ -69,6 +69,7 @@ require __DIR__ . "/../assets/sharedUserFunctions.php";
         <a href="./index.php"><button class="button" data-i18n="exit">Exit without saving</button></a>
         </main>
     </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="/assets/apiCommunication.js"></script>
     <script src="editUser.js"></script>
 </html>
