67a023776e4ae06e8813fd57d90ed1d450f08208
mymy1327 <my.lengoctra@edu.hyria.fi>
2026-09-24 20:59:36 +0300

    /

--- admin/user.php
+++ admin/user.php
@@ -54,7 +54,8 @@ require __DIR__ . "/../assets/sharedUserFunctions.php";
         </label>
 
         <select class="form-select" name="role" id="role">
-            <option value="admin" data-i18n="roleAdmin" selected>Admin</option>
+            <option selected>Choose your role...</option>
+            <option value="admin" data-i18n="roleAdmin">Admin</option>
             <option value="student" data-i18n="roleStudent">Student</option>
             <option value="teacher" data-i18n="roleTeacher">Teacher</option>
         </select>
