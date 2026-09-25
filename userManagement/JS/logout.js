75950d3fd732568ad6bf6fe694397972d5c99068
mymy1327 <my.lengoctra@edu.hyria.fi>
2026-09-25 09:44:08 +0300

    .

--- userManagement/JS/logout.js
+++
@@ -1,8 +0,0 @@
-//sessionStorage.clear("login");
-const params = new URLSearchParams(window.location.search);
-let uri = "./login.php";
-if(params.has("next")) {
-  uri += "?next=" + encodeURIComponent(params.get("next"))
-}
-console.log(uri);
-window.location.replace(uri)
